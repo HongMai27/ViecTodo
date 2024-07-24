322900b7e9c7714b80e5dd20957fdec6eda27d49
HongMai27 <mtran3827@gmail.com>
2024-07-24 16:57:03 +0700

    màu đỏ cho task hết hạn

--- gui/src/components/tasks/task.tsx
+++ gui/src/components/tasks/task.tsx
@@ -1,32 +1,23 @@
-
-
-import { useNavigation } from "@react-navigation/native"
-import React from "react"
-import { GestureResponderEvent, Pressable } from "react-native"
-import {
-  FadeInLeft,
-  FadeInRight,
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-} from "react-native-reanimated"
-import useSWRMutation from "swr/mutation"
-import { ICategory, ITask } from "../../types"
-import axiosInstance from "../../services/config"
-import { HomeScreenNavigationType } from "../../navigation/types"
-import { AnimatedBox, Box, Text } from "../../utils/theme"
-import Icon  from "react-native-vector-icons/Entypo"
-import { format } from "date-fns"
-
+import React, { useState } from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+import useSWRMutation from "swr/mutation";
+import { ICategory, ITask } from "../../types";
+import axiosInstance, { fetcher } from "../../services/config";
+import { HomeScreenNavigationType } from "../../navigation/types";
+import { useNavigation } from "@react-navigation/native";
+import { Box } from "../../utils/theme";
+import { isToday, format } from "date-fns";
+import useSWR from "swr";
 
 type TaskProps = {
   task: ITask;
-  mutateTasks: () => Promise<ITask[] | undefined>
-}
+  mutateTasks: () => Promise<ITask[] | undefined>;
+};
 
 interface ITaskStatusRequest {
-  id: string
-  isCompleted: boolean
+  id: string;
+  isCompleted: boolean;
 }
 
 const toggleTaskStatusRequest = async (
@@ -34,117 +25,177 @@ const toggleTaskStatusRequest = async (
   { arg }: { arg: ITaskStatusRequest }
 ) => {
   try {
-    await axiosInstance.put(url + "/" + arg.id, {
-      ...arg,
-    })
+    await axiosInstance.put(url + "/" + arg.id, { ...arg });
   } catch (error) {
-    console.log("error in toggleTaskStatusRequest", error)
-    throw error
+    console.log("error in toggleTaskStatusRequest", error);
+    throw error;
   }
-}
+};
 
 const Task = ({ task, mutateTasks }: TaskProps) => {
-  const { trigger } = useSWRMutation("tasks/update", toggleTaskStatusRequest)
-
-  const offset = useSharedValue(1)
-  const checkmarkIconSize = useSharedValue(0.8)
-
-  const formattedDate = format(new Date(task.date), 'dd/MM/yyyy');
-  const navigation = useNavigation<HomeScreenNavigationType>()
+  const { trigger } = useSWRMutation("tasks/update", toggleTaskStatusRequest);
+  const [updatedTask, setUpdatedTask] = useState(task);
+  const navigation = useNavigation<HomeScreenNavigationType>();
 
   const toggleTaskStatus = async () => {
     try {
       const _updatedTask = {
         id: task._id,
         isCompleted: !task.isCompleted,
-      }
-      await trigger(_updatedTask)
-      await mutateTasks()
-      if (!_updatedTask.isCompleted) {
-        offset.value = 1
-        checkmarkIconSize.value = 0
-      } else {
-        offset.value = 1.1
-        checkmarkIconSize.value = 1
-      }
+      };
+      await trigger(_updatedTask);
+      await mutateTasks();
     } catch (error) {
-      console.log("error in toggleTaskStatus", error)
-      throw error
+      console.log("error in toggleTaskStatus", error);
+      throw error;
     }
-  }
+  };
+  const isTaskExpired = (dueDate) => {
+    const today = new Date();
+    return new Date(dueDate) < today;
+  };
+  const navigateToEditTask = () => {
+    navigation.navigate("EditTask", { task });
+  };
 
- const navigateToEditTask = () => {
-    navigation.navigate("EditTask", {
-      task,
-    })
-  }
-
-
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: withSpring(offset.value) }],
-    }
-  })
-
-  const checkMarkIconStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: withSpring(checkmarkIconSize.value) }],
-      opacity: task.isCompleted ? offset.value : 0,
-    }
-  })
+  const truncateText = (text: string, maxLength: number) => {
+    return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
+  };
+  const { data: categories, isLoading } = useSWR<ICategory[]>(
+    "categories",
+    fetcher
+  );
+  
+  const selectedCategory = categories?.find(
+    (_category) => _category._id === updatedTask.categoryId
+  );
 
   return (
-    <AnimatedBox entering={FadeInRight} exiting={FadeInLeft}>
-     <Box
-        bg="gray100"
-        p="4"
-        borderRadius="rounded-2xl"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
+    <View style={styles.pressable}>
+      <View
+        style={[
+          styles.taskContainer,
+          {
+            backgroundColor: task.isCompleted ? "white" : "white",
+            borderColor: task.isCompleted ? "#DB3AFF" : "#d946ef",
+          
+          },
+        ]}
       >
-          <Box flexDirection="row" alignItems="center">
-            <AnimatedBox
-              style={[animatedStyles]}
-              flexDirection="row"
-              alignItems="center"
+        <View style={styles.taskContent}>
+          <Pressable
+            style={[
+              styles.checkmarkContainer,
+              {
+                backgroundColor: task.isCompleted ? "#DB3AFF" : "white",
+              },
+            ]}
+            onPress={toggleTaskStatus}
+          >
+            {task.isCompleted && <Icon name="check" size={20} color="#ffffff" />}
+          </Pressable>
+          <View style={styles.taskTextContainer}>
+            <Text
+              style={[
+                styles.taskText,
+                {
+                  color: task.isCompleted ? "black" : "black",
+                  // textDecorationLine: !task.isCompleted && isTaskExpired(task.date)  ? "line-through" : "none",
+                },
+              ]}
             >
-              <Pressable onPress={toggleTaskStatus}>
-              <Box
-                height={26}
-                width={26}
-                bg={task.isCompleted ? "gray9" : "gray300"}
-                borderRadius="rounded-xl"
-                alignItems="center"
-                justifyContent="center"
-              >
-                {task.isCompleted && (
-                  <AnimatedBox style={[checkMarkIconStyles]}>
-                    <Icon name="check" size={20} color="white" />
-                  </AnimatedBox>
-                )}
-              </Box>
-              </Pressable>
-            </AnimatedBox>
-            <Box flexDirection="column" flex={1} marginLeft={'10'}>
-          <Box flexDirection='row'>
-      <Text variant="textXl" fontWeight="500" mb="3">
-            {task.name}
-          </Text>
-      </Box>
-          <Text variant="textBase" fontWeight="600" mb="3">
-            {formattedDate}
-          </Text>
-        </Box>
-          </Box>
-          <Box>
-          <Pressable onPress={navigateToEditTask} style={{right: 10}}>
-              <Icon name="dots-three-vertical" size={16} color='#EB91FF' />
-            </Pressable>
-          </Box>
-        </Box>
-    </AnimatedBox>
-  )
-}
+              {truncateText(task.name, 20)}
+            </Text>
+            {selectedCategory && (
+              <Text style={[styles.categoryText, { color: selectedCategory.color.code }]}>
+                {truncateText(selectedCategory.name, 25)}
+              </Text>
+            )}
+          </View>
+        </View>
+        <View style={styles.dateContainer}>
+          <Pressable
+            style={{  
+            backgroundColor: !task.isCompleted && isTaskExpired(task.date) ? '#fca5a5' : '#bbf7d0',
+            borderRadius: 10,
+            height: 40,
+            width: 60,
+            right: -10
+          }}
+          >
+            <Text style={[styles.dateText, {textAlign:'center'}]}>
+              {isToday(new Date(task.date)) ? "Today" : format(new Date(task.date), "dd/MM")}
+            </Text>
+          </Pressable>
+        </View>
+        <Pressable onPress={navigateToEditTask} style={styles.editButton}>
+          <Icon name="dots-three-vertical" size={20} color="#555555" />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
-export default Task
+const styles = StyleSheet.create({
+  pressable: {
+    borderRadius: 10,
+    backgroundColor: '#f9f9f9',
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  taskContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+  taskContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+  },
+  checkmarkContainer: {
+    height: 28,
+    width: 28,
+    borderRadius: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 12,
+    elevation: 1,
+    borderWidth:1,
+    borderColor: '#DB3AFF'
+  },
+  taskTextContainer: {
+    flex: 1,
+  },
+  taskText: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  categoryText: {
+    fontSize: 16,
+    fontWeight: '400',
+    marginTop: 4,
+  },
+  dateContainer: {
+    marginRight: 12,
+  },
+  dateText: {
+    fontSize: 14,
+    color: "black",
+    justifyContent:'center',
+    textAlign:'center',
+    top: 10
+  },
+  editButton: {
+    padding: 8,
+  },
+});
+
+export default Task;
--- gui/src/screens/home-screen/index.tsx
+++ gui/src/screens/home-screen/index.tsx
@@ -8,135 +8,132 @@ import { ITask } from "../../types";
 import { fetcher, removeToken } from "../../services/config";
 import Loader from "../../components/shared/loader";
 import SafeAreaWrapper from "../../components/shared/safe-area-wrapper";
-import { AnimatedText, Box, Text } from "../../utils/theme";
+import theme, { AnimatedText, Box, Text } from "../../utils/theme";
 import Task from "../../components/tasks/task";
 import { format } from "date-fns-tz";
 import TaskActions from "../../components/tasks/task-action";
-import TaskBink from "../../components/tasks/task";
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { BottomModal, ModalContent, ModalTitle, SlideAnimation, ModalPortal } from 'react-native-modals';
+import { ModalPortal } from 'react-native-modals';
+import LinearGradient from 'react-native-linear-gradient';
 
 const today = new Date();
 const greeting = getGreeting({ hour: new Date().getHours() });
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   const { user } = useUserGlobalStore();
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredTasks, setFilteredTasks] = useState<ITask[]>([]);
   const [isModalVisible, setModalVisible] = useState(false);
-  const {
-    data: tasks,
-    isLoading,
-    mutate: mutateTasks,
-  } = useSWR<ITask[]>("tasks/", fetcher,
-    {
-      refreshInterval: 1000,
-    }
-  );
+  const { data: tasks, isLoading, mutate: mutateTasks } = useSWR<ITask[]>("tasks/", fetcher, {
+    refreshInterval: 1000,
+  });
+
   useEffect(() => {
     if (tasks) {
       setFilteredTasks(tasks.filter(task => task.name.toLowerCase().includes(searchQuery.toLowerCase())));
     }
   }, [tasks, searchQuery]);
+
   const handleSearchChange = (text: string) => {
     setSearchQuery(text);
   };
-  const pendingTasksCount = tasks.filter(task => !task.isCompleted).length;
+  const pendingTasksCount = tasks?.filter(task => !task.isCompleted).length ?? 0;
+  const sortedTasks = filteredTasks
+    .slice()
+    .sort((a, b) => {
+      const dueDateA = new Date(a.date);
+      const dueDateB = new Date(b.date);
+      if (a.isCompleted !== b.isCompleted) {
+        return a.isCompleted ? 1 : -1;
+      }
+      return a.isCompleted
+        ? dueDateB.getTime() - dueDateA.getTime() 
+        : dueDateA.getTime() - dueDateB.getTime(); 
+    })
+    .slice(0, 15); 
+
   if (isLoading || !tasks) {
     return <Loader />;
   }
-  
+
   return (
     <SafeAreaWrapper>
       <Box flex={1} mx="4" mt="4">
-        <Box flexDirection="row" alignItems="center" justifyContent="space-between">
-          <AnimatedText
-            variant="textXl"
-            fontWeight="500"
-            entering={ZoomInEasyDown.delay(500).duration(700)}
-          >
-             {greeting}, {''}
-            <Text color="purple1000">
-            {user?.name}
+        {/* Container chung cho lời chào, avatar và ô tìm kiếm */}
+        <Box flexDirection="column" alignItems="center" justifyContent="center">
+          <Box flexDirection="row" alignItems="center" justifyContent="space-between" width="100%" mb="1">
+            <AnimatedText
+              variant="text2Xl"
+              fontWeight="500"
+              entering={ZoomInEasyDown.delay(500).duration(700)}
+            >
+              {greeting}, {''}
+              <Text color="purple1000">
+                {user?.name}
+              </Text>
+            </AnimatedText>
+            <Pressable onPress={() => navigation.navigate('Profile')}>
+              <Image
+                source={{ uri: "https://picsum.photos/200" }}
+                style={styles.avatar}
+              />
+            </Pressable>
+          </Box>
+
+          <Box flexDirection="row" alignItems="center" width="100%" mb="1">
+            <Text variant="textBase" fontWeight="300">
+              Hôm nay, {format(today, "dd/MM/yyyy")} có {pendingTasksCount} việc chưa làm
             </Text>
-          </AnimatedText>
-          <Pressable onPress={() => navigation.navigate('Profile')}>
-            <Image
-              source={{ uri: "https://picsum.photos/200" }}
-              style={styles.avatar}
-            />
-          </Pressable>
+          </Box>
+
+          <Box width="100%" paddingVertical="1">
+            <View style={styles.searchContainer}>
+              <Icon name="search" size={20} color="#888" style={styles.searchIcon} />
+              <TextInput
+                style={styles.searchInput}
+                placeholder="Tìm kiếm công việc..."
+                placeholderTextColor="#888"
+                value={searchQuery}
+                onChangeText={handleSearchChange}
+              />
+            </View>
+          </Box>
         </Box>
 
-        <Box flexDirection="row" alignItems="center">
-          <Text variant="textLg" fontWeight="500">
-            Hôm nay, {format(today, "dd/MM/yyyy")} - {pendingTasksCount} việc chưa làm
-          </Text>
-        </Box>
-        <Box paddingHorizontal="4" paddingVertical="2" flexDirection="row">
-        <View style={[styles.searchContainer, ]}>
-          <Icon name="search" size={20} color="#888" style={styles.searchIcon} />
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Tìm kiếm..."
-            placeholderTextColor="#888"
-            value={searchQuery}
-            onChangeText={handleSearchChange}
-          />
-              <Pressable  onPress={() => setModalVisible(!isModalVisible)} style={{ right: -45 }}>
-        <Icon name="plus-circle" size={30} color="#EB91FF" />
-          </Pressable>
-          <TaskActions
-        categoryId=""
-        isModalVisible={isModalVisible}
-        setModalVisible={setModalVisible}
-      />            
-        </View>
-      </Box>
-    
         <Box height={10} />
         {tasks.length > 0 ? (
-        <FlatList
-          data={filteredTasks}
-          renderItem={({ item }) =>( 
-          <Task task={item} mutateTasks={mutateTasks} />
-          )
-        }
-          ItemSeparatorComponent={() => <Box height={14} />}
-          showsVerticalScrollIndicator={false}
-          keyExtractor={(item) => item._id}
-        />
-        )  : ( // return when 0 task
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "auto",
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}>
-          <Image
-            style={{ width: 200, height: 200, resizeMode: "contain" }}
-            source={{
-              uri: "https://cdn-icons-png.flaticon.com/128/6104/6104865.png",
-            }}
+          <FlatList
+            data={sortedTasks}
+            renderItem={({ item }) => <Task task={item} mutateTasks={mutateTasks} />}
+            ItemSeparatorComponent={() => <Box height={14} />}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item) => item._id}
           />
-          <Text
-            style={{fontSize: 18,marginTop: 15,fontWeight: "600",textAlign: "center", fontFamily:'Lato'}}
-          >
-            Danh sách công việc đang trống
-          </Text>
-          <Text
-            style={{fontSize: 13,marginTop: 15,fontWeight: "600",textAlign: "center",
-            }}
-          >
-            Nhấp + để thêm công việc mới
-          </Text>
-        </View>
-      )}
-        
+        ) : (
+          <View style={styles.emptyTaskContainer}>
+            <Image
+              style={styles.emptyTaskImage}
+              source={{ uri: "https://cdn-icons-png.flaticon.com/128/6104/6104865.png" }}
+            />
+            <Text style={styles.emptyTaskText}>Danh sách công việc đang trống</Text>
+            <Text style={styles.emptyTaskSubText}>Nhấp + để thêm công việc mới</Text>
+          </View>
+        )}
+
+        <TaskActions
+          categoryId=""
+          isModalVisible={isModalVisible}
+          setModalVisible={setModalVisible}
+        />
       </Box>
+
+      <Pressable
+        onPress={() => setModalVisible(!isModalVisible)}
+        style={[styles.container, styles.wrapper]}
+      >
+        <Icon  name="plus" size={28} color={theme.colors.white} />
+      </Pressable>
+
       <ModalPortal />
     </SafeAreaWrapper>
   );
@@ -148,17 +145,35 @@ const styles = StyleSheet.create({
   avatar: {
     width: 50,
     height: 50,
-    borderRadius: 25,
+    borderRadius: 50,
+    borderWidth: 1,
+  },
+  wrapper: {
+    position: 'absolute',
+    bottom: 0,
+    alignSelf: 'center',
+    margin: 10,
+  },
+  container: {
+    width: 60,
+    height: 60,
+    borderColor:'#DB3AFF',
+    borderWidth:1,
+    backgroundColor:'#f5d0fe',
+    borderRadius:50, 
+    alignItems:"center",
+    justifyContent:"center"
   },
   searchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-    borderRadius: 50,
-    left: -20,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
     paddingHorizontal: 10,
     paddingVertical: 5,
-    width: 350
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#d946e9',
   },
   searchIcon: {
     marginRight: 10,
@@ -166,6 +181,49 @@ const styles = StyleSheet.create({
   searchInput: {
     flex: 1,
     fontSize: 16,
-    
+  },
+  emptyTaskContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "auto",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  emptyTaskImage: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+  emptyTaskText: {
+    fontSize: 18,
+    marginTop: 15,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  emptyTaskSubText: {
+    fontSize: 13,
+    marginTop: 15,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  overlayButton: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    margin: 10,
+    zIndex: 0,
+  },
+  circleButton: {
+    width: 66,
+    height: 66,
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  plusText: {
+    fontSize: 45,
+    color: '#fff',
+    alignContent: 'center',
   },
 });
